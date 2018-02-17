74991e5da1901c2a1f2f43d43a010ff41f698632
nabin360 <nabinr231@gmail.com>
2018-02-17 17:23:59 +0600

    first commit

---
+++ app/src/main/java/com/example/xero/nabin/Main_Activity.java
@@ -0,0 +1,13 @@
+package com.example.xero.nabin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.material_design_login_page);
+    }
+}
